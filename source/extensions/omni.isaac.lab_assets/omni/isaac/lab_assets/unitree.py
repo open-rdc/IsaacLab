79ae3600fe0716942541991c5c8820cf0668c014
aiba0921 <s21C1004GV@s.chibakoudai.jp>
2024-11-23 14:14:36 +0900

    G1_CFG add knee

--- source/extensions/omni.isaac.lab_assets/omni/isaac/lab_assets/unitree.py
+++ source/extensions/omni.isaac.lab_assets/omni/isaac/lab_assets/unitree.py
@@ -11,9 +11,7 @@ The following configurations are available:
 * :obj:`UNITREE_GO1_CFG`: Unitree Go1 robot with actuator net model for the legs
 * :obj:`UNITREE_GO2_CFG`: Unitree Go2 robot with DC motor model for the legs
 * :obj:`H1_CFG`: H1 humanoid robot
-* :obj:`H1_MINIMAL_CFG`: H1 humanoid robot with minimal collision bodies
 * :obj:`G1_CFG`: G1 humanoid robot
-* :obj:`G1_MINIMAL_CFG`: G1 humanoid robot with minimal collision bodies
 
 Reference: https://github.com/unitreerobotics/unitree_ros
 """
@@ -312,7 +310,6 @@ G1_CFG = ArticulationCfg(
                 ".*_hip_yaw_joint",
                 ".*_hip_roll_joint",
                 ".*_hip_pitch_joint",
-                ".*_knee_joint",
                 "torso_joint",
             ],
             effort_limit=300,
@@ -321,22 +318,36 @@ G1_CFG = ArticulationCfg(
                 ".*_hip_yaw_joint": 150.0,
                 ".*_hip_roll_joint": 150.0,
                 ".*_hip_pitch_joint": 200.0,
-                ".*_knee_joint": 200.0,
                 "torso_joint": 200.0,
             },
             damping={
                 ".*_hip_yaw_joint": 5.0,
                 ".*_hip_roll_joint": 5.0,
                 ".*_hip_pitch_joint": 5.0,
-                ".*_knee_joint": 5.0,
                 "torso_joint": 5.0,
             },
             armature={
                 ".*_hip_.*": 0.01,
-                ".*_knee_joint": 0.01,
                 "torso_joint": 0.01,
             },
         ),
+        "knee": ImplicitActuatorCfg(
+            joint_names_expr=[
+                ".*_knee_joint",
+            ],
+            effort_limit=300,
+            velocity_limit=100.0,
+            stiffness={
+                ".*_knee_joint": 200.0,
+            },
+            damping={
+                ".*_knee_joint": 5.0,
+            },
+            armature={
+                ".*_knee_joint": 0.01,
+            },
+        ),
+
         "feet": ImplicitActuatorCfg(
             effort_limit=20,
             joint_names_expr=[".*_ankle_pitch_joint", ".*_ankle_roll_joint"],
